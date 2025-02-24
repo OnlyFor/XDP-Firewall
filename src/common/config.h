6194135ce6c6171e09c4e3693287e540f5a76029
OnlyFor <OnlyFor@users.noreply.github.com>
2025-02-24 09:01:18 +0800

    Update config.h

--- src/common/config.h
+++ src/common/config.h
@@ -2,7 +2,7 @@
 
 // The maximum amount of filters allowed.
 // Decrease this value if you receive errors related to the BPF program being too large.
-#define MAX_FILTERS 60
+#define MAX_FILTERS 90
 
 // The maximum amount of IPs/flows to track stats for.
 // The higher this value is, the more memory that'll be used.
@@ -10,7 +10,7 @@
 
 // Feel free to comment this out if you don't want the `blocked` entry on the stats map to be incremented every single time a packet is dropped from the source IP being on the blocked map.
 // Commenting this line out should increase performance when blocking malicious traffic.
-#define DO_STATS_ON_BLOCK_MAP
+// #define DO_STATS_ON_BLOCK_MAP
 
 // When this is defined, a check will occur inside the IPv4 and IPv6 filters.
 // For IPv6 packets, if no IPv6 source/destination IP addresses are set, but there is an IPv4 address, it will ignore the filter.
